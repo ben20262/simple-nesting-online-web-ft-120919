0ba5d88b08b13a0989d5c60cd52f089cbb992bff
Benjamin Small <ben20262@aim.com>
2019-12-11 23:52:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -35,6 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+	programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -53,6 +54,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+	programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -77,8 +79,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-
+	programmer_hash[:yukihiro_matsumoto] = {
+		:known_for => "Ruby",
+		:languages => ["LISP", "C"]
+	}
+	programmer_hash
 end
 
 def changing_alan
